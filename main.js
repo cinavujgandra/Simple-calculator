dab8341f7669cbdd19d95ad2471e99a0eff843c9
cinavujgandra <cinavujgandra@gmail.com>
2023-02-09 11:53:22 +0100

    .

---
+++ main.js
@@ -0,0 +1,19 @@
+let sumStr = ""
+let result1 = document.querySelector(".result1");
+let result2 = document.querySelector(".result2");
+
+const char = (e) => {
+    sumStr = sumStr.concat(e.dataset.value)
+    result1.innerHTML = sumStr
+}
+
+const equal = () => {
+    result2.innerHTML = `=${eval(sumStr)}`
+}
+
+
+const cls = () => {
+    sumStr = ""
+    result1.innerHTML = ""
+    result2.innerHTML = ""
+}
